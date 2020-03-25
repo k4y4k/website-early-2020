0a372f85b992bc5cae20999420f6256a7215fe9d
kayak <kayak@kayak.rocks>
2020-03-25 11:09:14 +1000

    refactor: hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,14 @@
 import React from 'react'
-import Title from '../components/index/Title'
 import Section from '../components/common/Section'
-import Subtitle from '../components/index/Subtitle'
-import Button from '../components/common/Button'
-import { Github, Twitter } from '@icons-pack/react-simple-icons'
 import CardContainer from '../components/cards/CardContainer'
 import BlogListing from '../components/blog/BlogListing'
 import Footer from '../components/index/Footer'
+import HeroContainer from '../components/index/HeroContainer'
 
 export default () => (
   <React.Fragment>
     <Section centred={true}>
-      <div>
-        <Title />
-        <Subtitle />
-
-        <div className='flex justify-center mt-4 mb-4'>
-          <Button>
-            <a href='https://github.com/k4y4k'>
-              <Github />
-            </a>
-          </Button>
-          <Button>
-            <a href='https://twitter.com/__kayak__'>
-              <Twitter />
-            </a>
-          </Button>
-          <Button filled>
-            <a href='mailto:kayak@kayak.rocks'>get in touch</a>
-          </Button>
-        </div>
-      </div>
+      <HeroContainer />
     </Section>
     <Section>
       <p>my name is kayak, i make websites, please hire me</p>
