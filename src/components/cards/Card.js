07dbdc9d7a1e81e19e24a6d0e4964c910f970c89
kayak <kayak@kayak.rocks>
2020-03-26 10:32:58 +1000

    fix: add hyphenation to cards on smaller screen sizes

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -3,20 +3,30 @@ import styled from 'styled-components'
 import {
   ReactJs,
   Javascript,
-  Mongodb,
   Electron,
   Tailwindcss,
   Sass,
 } from '@icons-pack/react-simple-icons'
 
-const cardContainerClasses = ['border', 'border-purple-600', 'm-4', 'p-4']
+const cardContainerClasses = [
+  'border',
+  'border-purple-600',
+  'm-4',
+  'p-4',
+  'w-full',
+  'md:w-auto',
+]
 
 const usesClasses = ['font-black', 'text-sm', 'uppercase', 'p-1', 'm-1']
 
+const Description = styled.p`
+  hyphens: auto;
+`
+
 const UnstyledCard = ({ className, children, name, description, uses }) => (
-  <div className={`${className} ${cardContainerClasses.join(' ')}`}>
+  <div lang='en' className={`${className} ${cardContainerClasses.join(' ')}`}>
     <h3 className='text-lg font-bold font-display mb-2'>{name}</h3>
-    <p className='mb-4'>{description}</p>
+    <Description className='mb-4'>{description}</Description>
 
     <hr className='bg-purple-600 mb-4 border-0 h-px' />
 
@@ -25,7 +35,6 @@ const UnstyledCard = ({ className, children, name, description, uses }) => (
         <p className={usesClasses.join(' ')}>
           {use === 'React' ? <ReactJs color='#61DAFB' /> : null}
           {use === 'JavaScript' ? <Javascript color='#F7DF1E' /> : null}
-          {use === 'mongoDB' ? <Mongodb color='#47A248' /> : null}
           {use === 'Electron' ? <Electron color='#47848F' /> : null}
           {use === 'tailwindCSS' ? <Tailwindcss color='#38B2AC' /> : null}
           {use === 'Sass' ? <Sass color='#CC6699' /> : null}
@@ -36,5 +45,5 @@ const UnstyledCard = ({ className, children, name, description, uses }) => (
 )
 
 export default styled(UnstyledCard)`
-  max-width: 30vw;
+  max-width: 480px;
 `
